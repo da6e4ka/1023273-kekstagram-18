09dfe2ef630c60d8afce48519fea1068e7529bc0
da6e4ka <d.a6.e.4k.a.s.v@gmail.com>
2019-10-08 14:05:36 +0300

    Fix eslint

--- js/main.js
+++ js/main.js
@@ -1,25 +1,27 @@
+'use strict';
+
 window.getRandomInt = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max + 1 - min)) + min;
 };
 
-window.getRandomArrayElement = function(arr) {
-  return arr[Math.floor(arr.length * Math.random())]
+window.getRandomArrayElement = function (arr) {
+  return arr[Math.floor(arr.length * Math.random())];
 };
 
 var pictures = document.querySelector('.pictures');
 
-var generatePhotosMock = function() {
+var generatePhotosMock = function () {
   var mocks = [];
   for (var i = 1; i <= 25; i++) {
-    mocks.push(getPhotoMock(i))
+    mocks.push(getPhotoMock(i));
   }
   return mocks;
 };
 
-var getPhotoMock = function(index) {
-  const commentVariants = [
+var getPhotoMock = function (index) {
+  var commentVariants = [
     'Всё отлично!',
     'В целом всё неплохо. Но не всё.',
     'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -28,7 +30,7 @@ var getPhotoMock = function(index) {
     'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
   ];
 
-  const descriptionVariants = [
+  var descriptionVariants = [
     'Ну типа фотка',
     'Еще сфоткал вот смотрите',
     'Продам гараж'
@@ -36,23 +38,23 @@ var getPhotoMock = function(index) {
 
 
   var comments = [];
-  for (let i = 0; i < window.getRandomInt(3, 10); i++) {
-    comments.push(window.getRandomArrayElement(commentVariants))
+  for (var i = 0; i < window.getRandomInt(3, 10); i++) {
+    comments.push(window.getRandomArrayElement(commentVariants));
   }
 
   return {
-    url: 'photos/'+index+'.jpg',
+    url: 'photos/' + index + '.jpg',
     description: window.getRandomArrayElement(descriptionVariants),
     likes: window.getRandomInt(15, 200),
     comments: comments,
-  }
+  };
 };
 
 
 function renderImages(photos) {
   var fragment = document.createDocumentFragment();
 
-  for (let i = 0; i < photos.length; i++) {
+  for (var i = 0; i < photos.length; i++) {
     fragment.appendChild(renderTemplate(photos[i]));
   }
 
