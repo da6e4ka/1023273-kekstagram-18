50bbb33c41e82ae2e4384440fdfbf7c5eefef1ca
da6e4ka <d.a6.e.4k.a.s.v@gmail.com>
2019-11-09 18:16:08 +0300

    Module7 task1

--- js/helpers.js
+++ js/helpers.js
@@ -1,22 +1,23 @@
 'use strict';
 
-(function () {
-
-  var showItem = function (item) {
-    item.classList.remove('hidden');
-  };
-
-  var hideItem = function (item) {
-    item.classList.add('hidden');
-  };
-
-  var getRandomNumber = function (min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
-  window.helpers = {
-    showItem: showItem,
-    hideItem: hideItem,
-    getRandomNumber: getRandomNumber
+window.helpers = (function () {
+  return {
+    getRandomValue: function (arr) {
+      return arr[Math.floor(arr.length * Math.random())];
+    },
+    getMaxElement: function (arr) {
+      var maxElement = arr[0];
+      for (var i = 1; i < arr.length; i++) {
+        if (maxElement < arr[i]) {
+          maxElement = arr[i];
+        }
+      }
+      return maxElement;
+    },
+    getRandomInt: function (min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
   };
 })();
