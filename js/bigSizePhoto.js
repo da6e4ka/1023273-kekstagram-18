b887a2a17290b10f5d1da1e339fa63859b2f0ac6
da6e4ka <d.a6.e.4k.a.s.v@gmail.com>
2019-11-17 18:23:20 +0300

    Fix bock hidden when comment in focus

--- js/bigSizePhoto.js
+++ js/bigSizePhoto.js
@@ -1,6 +1,12 @@
 'use strict';
 
 (function () {
+  var commentElement = document.querySelector('.social__footer-text');
+
+  var preventHandler = function(evt) {
+    evt.stopPropagation();
+  };
+
   var onPopupClose = function (evt) {
     if (evt.keyCode === window.constants.ESC_KEYCODE) {
       closeBigPhoto();
@@ -10,9 +16,11 @@
   var closeBigPhoto = function () {
     window.helpers.hideElement(window.preview.bigPictureElement);
     document.removeEventListener('keydown', onPopupClose);
+    commentElement.removeEventListener('keydown', preventHandler);
   };
 
   var showBigPhoto = function (data) {
+    commentElement.addEventListener('keydown', preventHandler);
     window.helpers.showElement(window.preview.bigPictureElement);
     window.preview.bigPictureElement.querySelector('.big-picture__img').querySelector('img').src = data.url;
 
